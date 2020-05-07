f9ec3aaa42d121d29047e8a0e0a2256029afa7a4
tkdtmy <tkdtmy.academy.0605@gmail.com>
2020-05-07 02:57:57 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_030504) do
+ActiveRecord::Schema.define(version: 2020_05_06_032707) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
