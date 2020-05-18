375030cc964a71e4d1e3a3c94cb9009f48dff79f
tkdtmy <tkdtmy.academy.0605@gmail.com>
2020-05-18 09:03:41 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "tasks#index"
+  get 'toppages/index'
+  root to: "toppages#index"
   
   resources :tasks
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :new, :create, :show]
 end
